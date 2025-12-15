59e645109e658a3d9b11f5db94df0473360334ca
hirosugu415 <kurowassan55555@gmail.com>
2025-12-15 11:17:56 +0900

    add Block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use sha2::{Sha256, Digest};
+
+#[derive(Debug)]
+struct Block {
+    id: u64,
+    hash: String,
+    previous_hash: String,
+    timestamp: u64,
+    data: String,
+    nonce: u64,
+}
+
+impl Block {
+    fn calculate_hash(&self)->String{
+        let formatted_block_data = format!("{}{}{}{}{}", self.id, self.previous_hash,self.timestamp, self.data, self.nonce);
+
+        let mut hasher = Sha256::new();
+        hasher.update(formatted_block_data);
+        let result = hasher.finalize();
+
+        hex::encode(result)
+    }
+
+    fn 
+}
+
 fn main(){
-    println!("Hello World!");
+    let genesis_block = Block {
+        id: 0,
+        hash: String::from("000abc..."),
+        previous_hash: String::from(""),
+        timestamp: 163400000,
+        data: String::from("Genesis Block"),
+        nonce: 0,
+    };
+
+    let hash = genesis_block.calculate_hash();
+
+    println!("Genesis Block: {}", hash);
 }
